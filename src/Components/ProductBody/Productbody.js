b76a0d719426459fbbc9120e018e27454fbd388e
AnikKDev <anikdev2016@gmail.com>
2022-03-26 15:10:34 +0600

    completed remove and choose device with some error handling

--- src/Components/ProductBody/Productbody.js
+++ src/Components/ProductBody/Productbody.js
@@ -13,8 +13,19 @@ const Productbody = () => {
 
     const [phoneData, setPhoneData] = useState([])
     const addToCartBtn = (product) => {
-        const newPhoneData = [...phoneData, product]
-        setPhoneData(newPhoneData)
+        const found = phoneData.find(phone => phone.id === product.id);
+        if (phoneData.length < 4) {
+            if (!found) {
+                const newPhoneData = [...phoneData, product]
+                setPhoneData(newPhoneData);
+            }
+        }
+        // else {
+        //     console.log('more than 4')
+        // }
+        // else {
+        //     console.log('dupli')
+        // }
     };
 
     const [selectedDevice, setSelectedDevice] = useState([]);
@@ -24,7 +35,7 @@ const Productbody = () => {
     }
 
     const chooseAgain = () => {
-        setSelectedDevice([]);
+        setPhoneData([]);
     }
     // console.log(phoneData.length);
     // console.log(showProducts)
@@ -46,16 +57,20 @@ const Productbody = () => {
             </div>
             <div className='col-md-4 selected-item-container-parent'>
                 <div className="selected-item-container">
-                    <h2>selected Items</h2>
+                    <h2 className='mb-4 mt-4 pt-3 text-center'>Selected Items</h2>
                     {phoneData.map(phone => <SelectedDevice
                         key={phone.id}
                         phone={phone}
                     ></SelectedDevice>)}
-                    <div className="d-flex flex-column align-items-center">
+                    <div className="d-flex flex-column align-items-center pb-5">
                         <div>
-                            <h4>{selectedDevice.name}</h4>
+                            <div className='d-flex justify-content-between px-5 align-items-center mb-3'>
+                                <img className='selected-device-img' src={selectedDevice?.img} alt="" />
+                                <h6>{selectedDevice?.name}</h6>
+                            </div>
+                            {/* <h4>{selectedDevice?.name}</h4> */}
                         </div>
-                        <button onClick={() => generateRandomPhone(phoneData)} className='choose-item-btn'>CHOOSE ONE FOR ME</button>
+                        <button onClick={() => generateRandomPhone(phoneData)} className='choose-item-btn mb-2'>CHOOSE ONE FOR ME</button>
                         <button onClick={() => chooseAgain(selectedDevice)} className='choose-item-btn'>CHOOSE AGAIN</button>
                     </div>
                 </div>
